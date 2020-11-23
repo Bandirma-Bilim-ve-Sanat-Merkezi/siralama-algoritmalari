2a6420d6df827f88b511cc58a6ab0359fc6f51c0
Xeno <68283330+xenopeltis1@users.noreply.github.com>
2020-11-23 15:55:20 +0300

    Create insertion.py

---
+++ insertion.py
@@ -0,0 +1,19 @@
+def insert(listt): 
+    
+    #birden başlayarak listenin uzunluğuna kadar gidecek bir döngü
+    for _ in range(1, len(listt)): 
+        
+        #bulunduğumuz geçiş
+        sira = arr[_] 
+  
+        esk = _-1
+        while sira < listt[esk] and esk >= 0: 
+                listt[esk + 1] = listt[esk] 
+                esk -= 1
+        listt[esk + 1] = sira
+        
+    return listt
+
+listt = [12, 11, 13, 5, 6] 
+arr = insert(listt)
+print(arr)
